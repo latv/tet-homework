02a3eb8ecfbb0c9176e236e54f10087c5eb58556
Jānis Feldmanis <janis_feldmanis@outlook.com>
2025-12-07 20:41:13 +0200

    added coupons

--- coupons/routes/api.php
+++ coupons/routes/api.php
@@ -1,22 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Tet\Coupons\Controllers\CouponController;
+use App\Http\Controllers\CouponController;
 
-Route::prefix('api/coupons')->group(function () {
+Route::prefix('/')->group(function () {
     // List all coupons
-    Route::get('/', [CouponController::class, 'index']);
+    Route::get('coupons', [CouponController::class, 'index']);
 
     // Create a new coupon
-    Route::post('/', [CouponController::class, 'store']);
+    Route::post('coupons', [CouponController::class, 'store']);
 
     // Show a specific coupon
-    Route::get('/{id}', [CouponController::class, 'show']);
+    Route::get('coupons/{id}', [CouponController::class, 'show']);
 
     // Update a coupon
-    Route::put('/{id}', [CouponController::class, 'update']);
-    Route::patch('/{id}', [CouponController::class, 'update']);
+    Route::put('coupons/{id}', [CouponController::class, 'update']);
+    Route::patch('coupons/{id}', [CouponController::class, 'update']);
 
     // Delete a coupon
-    Route::delete('/{id}', [CouponController::class, 'destroy']);
+    Route::delete('coupons/{id}', [CouponController::class, 'destroy']);
 });
